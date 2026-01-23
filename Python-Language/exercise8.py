a2700d729f8e357dd245c21fe4a791e4f861847d
Olgasec <olga.florou.sec@gmail.com>
2026-01-23 04:26:34 +0200

    Create exercise8.py

---
+++ Python-Language/exercise8.py
@@ -0,0 +1,9 @@
+secret_number = 7
+guess = 0
+while guess != secret_number:
+    guess = int(input("Guess the secret number (1-10): "))
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+print("Congratulations! You found it!")
