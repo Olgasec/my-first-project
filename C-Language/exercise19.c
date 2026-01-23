e5c5c9077bcfc9f3ab1ccde796d6eb949c17d7d1
Olgasec <olga.florou.sec@gmail.com>
2026-01-23 04:05:12 +0200

    Create exercise19.c

---
+++ C-Language/exercise19.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#define N 100
+void main()
+{
+char str[N], ch;
+int i, j, l;
+printf("\n\nSort a string array in ascending order :\n");
+printf("--------------------------------------------\n");
+printf("Input the string : ");
+fgets(str, N, stdin);
+l = strlen(str);
+for (i = 1; i < l; i++)
+for (j = 0; j < l - i; j++)
+if (str[j] > str[j + 1])
+{
+ch = str[j];
+str[j] = str[j + 1];
+str[j + 1] = ch;
+}
+printf("After sorting the string appears like : \n");
+printf("%s\n\n", str);
+}
