9dc617839943fc49cba5ad0174e4f39729df0b68
Olgasec <olga.florou.sec@gmail.com>
2026-01-23 04:23:25 +0200

    Create Exercise1.py

---
+++ Python-Language/Exercise1.py
@@ -0,0 +1,5 @@
+grade1 = float(input("Enter first grade: "))
+grade2 = float(input("Enter second grade: "))
+grade3 = float(input("Enter third grade: "))
+average = (grade1 + grade2 + grade3) / 3
+print(f"The average grade is: {average:.2f}")
